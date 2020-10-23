365c4dd32296b11fe4a0cbbb51a2abdd54f29080
Alayoubi,Ranim R <r.alayoubi@student.fontys.nl>
2020-10-23 20:41:57 +0200

    Filter books by selected language is implemented and data are illustarted

--- BookyWebAppClient/src/app/filter-users/filter-users.component.ts
+++ BookyWebAppClient/src/app/filter-users/filter-users.component.ts
@@ -18,21 +18,38 @@ export class FilterUsersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectionRadio: String;
+  selectionDrop: String;
+
   books: Book[]; 
   book: Book;
 
-  languages: object[] = [
-    {value: 'AR', viewValue: 'Arabic'},
-    {value: 'EN', viewValue: 'English'},
-    {value: 'FR', viewValue: 'French'}
-  ];
-
-  getBooksByFantasyType(){
-    this.filterService.filterBooksByFantasyType('Fantasy').subscribe((data)=>
+  getBooksByEnglishLanguage(){
+    this.filterService.filterBooksByLanguage('EN').subscribe((data)=>
     {
       this.books=<Book[]>data;
       console.log(this.books);      
     });
+  }
+
+  getBooksByFranceLanguage(){
+    this.filterService.filterBooksByLanguage('FR').subscribe((data)=>
+    {
+      this.books=<Book[]>data;
+      console.log(this.books);      
+    });
+  }
+
+  getBooksByArabicLanguage(){
+    this.filterService.filterBooksByLanguage('AR').subscribe((data)=>
+    {
+      this.books=<Book[]>data;
+      console.log(this.books);      
+    });
+  }
+
+  foundDataRadio(){
+    return this.selectionRadio;
   }
 
 
